0104697667e092caaa18d698263f70f9e69e781f
Exau2010 <olaezose@gmail.com>
2026-02-28 21:22:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// LOGIN
 async function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -7,16 +8,14 @@ async function login() {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
   });
-
   const data = await res.json();
   if (data.error) return alert(data.error);
 
-  localStorage.setItem("token", data.token);
   localStorage.setItem("user", username);
-
-  location.href = "chat.html";
+  window.location.href = "home.html";
 }
 
+// REGISTER
 async function register() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -26,16 +25,17 @@ async function register() {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
   });
-
   const data = await res.json();
   if (data.error) return alert(data.error);
 
   alert("Compte créé !");
-  location.href = "index.html";
+  window.location.href = "index.html";
 }
 
+// LOGOUT
 function logout() {
-  localStorage.removeItem("token");
   localStorage.removeItem("user");
-  location.href = "index.html";
-}
+  localStorage.removeItem("toUser");
+  localStorage.removeItem("selectedUser");
+  window.location.href = "index.html";
+    }
